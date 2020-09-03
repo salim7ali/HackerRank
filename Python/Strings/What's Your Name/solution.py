14849a2ac7758d64db749d3526b0f32e8744ce4e
Salim Ahmed Ali <salim7ali@gmail.com>
2020-09-04 00:32:45 +0530

    very easy python question

---
+++ Python/Strings/What's Your Name/solution.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem?h_r=profile
+
+ def print_full_name(a, b):
+    print ""
+
+if __name__ == '__main__':
+    first_name = raw_input()
+    last_name = raw_input()
+    print_full_name(first_name, last_name)
