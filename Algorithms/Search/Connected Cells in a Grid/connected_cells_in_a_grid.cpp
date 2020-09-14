72e971de2ba0d897ddde169d5e8712091ae6b0fb
Salim Ahmed Ali <salim7ali@gmail.com>
2020-09-14 10:06:04 +0530

    added more comments

--- Algorithms/Search/Connected Cells in a Grid/connected_cells_in_a_grid.cpp
+++ Algorithms/Search/Connected Cells in a Grid/connected_cells_in_a_grid.cpp
@@ -1,4 +1,10 @@
 // https://www.hackerrank.com/challenges/connected-cell-in-a-grid/problem
+// cases 0 to 7 represent different adjacent locations being checked for DFS
+// -|-|-|-|-
+// -|0|1|2|-
+// -|7|Q|3|-
+// -|6|5|4|-
+// -|-|-|-|-
 // #include<bits/stdc++.h>
 #include<vector>
 #include<iostream>
@@ -15,6 +21,7 @@ void modifiedDFS(vector<vector<int>> &matrix, int i, int j, int &currConnCount){
     matrix[i][j] = 2; // assigning value 2 if visited 
     currConnCount += 1;
 
+    // cases 0 to 2
     if(isValid(matrix, i-1, j-1) && matrix[i-1][j-1] == 1)//valid and unvisited
         modifiedDFS(matrix, i-1, j-1, currConnCount);
     if(isValid(matrix, i-1, j) && matrix[i-1][j] == 1)
@@ -22,9 +29,11 @@ void modifiedDFS(vector<vector<int>> &matrix, int i, int j, int &currConnCount){
     if(isValid(matrix, i-1, j+1) && matrix[i-1][j+1] == 1)
         modifiedDFS(matrix, i-1, j+1, currConnCount);
         
+    // case 3
     if(isValid(matrix, i, j+1) && matrix[i][j+1] == 1)
         modifiedDFS(matrix, i, j+1, currConnCount);
 
+    // cases 4 to 6
     if(isValid(matrix, i+1, j+1) && matrix[i+1][j+1] == 1)
         modifiedDFS(matrix, i+1, j+1, currConnCount);
     if(isValid(matrix, i+1, j) && matrix[i+1][j] == 1)
@@ -32,6 +41,7 @@ void modifiedDFS(vector<vector<int>> &matrix, int i, int j, int &currConnCount){
     if(isValid(matrix, i+1, j-1) && matrix[i+1][j-1] == 1)
         modifiedDFS(matrix, i+1, j-1, currConnCount);
 
+    // case 7
     if(isValid(matrix, i, j-1) && matrix[i][j-1] == 1)
         modifiedDFS(matrix, i, j-1, currConnCount);
 }
