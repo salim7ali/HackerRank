47493ac8897c0418cf3416e35fe858cf71d5924a
Salim Ahmed Ali <salim7ali@gmail.com>
2020-09-19 22:08:21 +0530

    solving Sock_merchant problem

---
+++ Algorithms/Implementation/Sock Merchant/Sock_Merchant.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem
+#include<vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+int sockMerchant(int n, vector<int> ar) {
+
+
+}
+
+int main(){
+    
+}
+
