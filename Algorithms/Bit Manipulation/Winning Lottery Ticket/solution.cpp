cf40e86816523023c27b9f78e8dee24310dd57cd
Salim Ahmed Ali <salim7ali@gmail.com>
2020-10-25 01:33:13 +0530

    tried optimizations. Not good enough. Working on it

--- Algorithms/Bit Manipulation/Winning Lottery Ticket/solution.cpp
+++ Algorithms/Bit Manipulation/Winning Lottery Ticket/solution.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 long winningLotteryTicket(vector<string> tickets) {
     vector<vector<bool>> digitExists(tickets.size(), vector<bool> (10, 0));
+    vector<int> countOfDigits(tickets.size());
 
     int digit;
     int digitsPresentInTicket = 0;
     for(int i=0; i<tickets.size(); i++){
-        for(int j=0; j<tickets[i].length(); j++){//digits in each ticket
+        for(int j=0; j<tickets[i].length(); j++){//each digit in ticket chosen
             digit = ((int)(tickets[i][j]) - 48);
             if(digitExists[i][digit] == false){
                 digitExists[i][digit] = true;
@@ -18,30 +19,34 @@ long winningLotteryTicket(vector<string> tickets) {
             if(digitsPresentInTicket == 10)
                 break;
         }
+        countOfDigits[i] = digitsPresentInTicket;
         digitsPresentInTicket = 0;//reset
     }
     
-    // for(int i=0; i<digitExists.size(); i++){
-    //     for(int j=0; j<digitExists[0].size(); j++){
-    //         cout<<digitExists[i][j]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
+    for(int i=0; i<digitExists.size(); i++){
+        for(int j=0; j<digitExists[0].size(); j++){
+            cout<<digitExists[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
 
     long noOfPairs = 0;
     int countOfUniqueDigits = 0;
     for(int i=0; i<tickets.size(); i++){
         for(int j=i+1; j<tickets.size(); j++){
-            // cout<<tickets[i]<<" "<<tickets[j]<<"\n";    
-            for(int k=0; k<10; k++){
-                if((digitExists[i][k] == true) || (digitExists[j][k] == true))
-                    countOfUniqueDigits += 1;
-            }
-            if(countOfUniqueDigits == 10)
-                noOfPairs += 1;
-            countOfUniqueDigits = 0;//reset count
+            // cout<<tickets[i]<<" "<<tickets[j]<<"\n";
+            if(countOfDigits[i] + countOfDigits[j] >= 10){
+                for(int k=0; k<10; k++){
+                    if((digitExists[i][k] == true) || (digitExists[j][k] == true))
+                        countOfUniqueDigits += 1;
+                }
+                if(countOfUniqueDigits == 10)
+                    noOfPairs += 1;
+                countOfUniqueDigits = 0;//reset count
+            }    
         }
     }
+
     return noOfPairs;
 }
 
