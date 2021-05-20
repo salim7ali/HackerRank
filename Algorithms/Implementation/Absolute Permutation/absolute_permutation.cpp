c5acb9a331eb32294e9de698afe414e80a86bee2
Salim <salim7ali@gmail.com>
2021-05-20 21:30:20 +0530

    solved medium level problem

--- Algorithms/Implementation/Absolute Permutation/absolute_permutation.cpp
+++ Algorithms/Implementation/Absolute Permutation/absolute_permutation.cpp
@@ -2,10 +2,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
 vector<int> absolutePermutation(int n, int k) {
+    vector<int> result(n);
 
+    bool i_plus_k = true;
+    int run =0;
+    int result_sum = 0;
+    for(int i=0; i<result.size(); i++){
+        // i+k or i-k
+
+        if(i_plus_k)
+            result[i] = (i+1)+k;
+        else
+            result[i] = (i+1)-k;
+        run += 1;
+        
+        if(run ==k){
+            i_plus_k = !i_plus_k;
+            run =0;
+        }
+        result_sum += result[i];
+    }
+    int sum_N = (n*(n+1))/2;
+    if(sum_N != result_sum)
+        result = {-1};
+
+    return result;
 }
 
 void disp_result(vector<int> nums){
@@ -26,6 +48,9 @@ int main(){
     disp_result(result);
 
     result = absolutePermutation(3, 2);
+    disp_result(result);
+
+    result = absolutePermutation(100, 2);
     disp_result(result);
 
 }
