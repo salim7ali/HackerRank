e03e3fd99514d2d74d643040fea6bda843ac72e6
Salim <salim7ali@gmail.com>
2021-06-07 23:00:58 +0530

    First commit. Added boiler plate code.

---
+++ A/Reverse_singly_linked_list/reverse_singly_linked_list.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int value;
+    node *next;
+};
+
+class SinglyLinkedList{
+public:
+    node* recursiveReverse(){
+        
+    }
+    int iterativeReverse(){
+
+    }
+};
+
+int main(){
+    
+}
