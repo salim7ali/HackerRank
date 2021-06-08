3ed538734d78448758d0a951931827784d52e945
Salim <salim7ali@gmail.com>
2021-06-08 22:02:55 +0530

    Minor changes.

--- A/Reverse_singly_linked_list/reverse_singly_linked_list.cpp
+++ A/Reverse_singly_linked_list/reverse_singly_linked_list.cpp
@@ -6,16 +6,39 @@ struct node{
     node *next;
 };
 
+// head->
+// 5->3->20->NULL
 class SinglyLinkedList{
+    
 public:
-    node* recursiveReverse(){
+    node* recursiveReverse(node *currPtr){
+
+        if(currPtr->next){
+            recursiveReverse(currPtr->next);
+        }
         
     }
+
     int iterativeReverse(){
+        node *runningPtr = head;
+
+        while(runningPtr){
+            runningPtr = runningPtr->next;
+        }
+
+    }
+
+    void addItem(){
 
     }
 };
 
 int main(){
     
+    SinglyLinkedList obj;
+
+    // insert elements into LL
+
+
+    obj.recursiveReverse(obj.head);
 }
