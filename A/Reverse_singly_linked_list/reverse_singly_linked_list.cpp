8eb8494deaafe7938718497ca21037bf20bf5f19
Salim <salim7ali@gmail.com>
2021-06-09 13:49:59 +0530

    Completed function for recursive reverse.

--- A/Reverse_singly_linked_list/reverse_singly_linked_list.cpp
+++ A/Reverse_singly_linked_list/reverse_singly_linked_list.cpp
@@ -8,15 +8,25 @@ struct node{
 
 // head->
 // 5->3->20->NULL
+
+// head->
+// 5-> <-3<-20 
 class SinglyLinkedList{
-    
+    node *oldHead;
+    node *newHead;
 public:
-    node* recursiveReverse(node *currPtr){
+    node* recursiveReverse(node *currPtr, node *prevPtr){
 
         if(currPtr->next){
-            recursiveReverse(currPtr->next);
+            recursiveReverse(currPtr->next, currPtr);
+        }else{// end node to be set as start node
+            newHead = currPtr;
         }
-        
+
+        if(currPtr != oldHead)
+            currPtr->next = prevPtr;
+        else    // new end node
+            currPtr->next = NULL;
     }
 
     int iterativeReverse(){
